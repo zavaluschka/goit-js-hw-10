1f1617cd69e7c484aa20c2261260ff2328e9ac09
zavaluschka <38804687+zavaluschka@users.noreply.github.com>
2023-08-22 18:08:32 +0300

    home001

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,14 @@
 
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const body = document.querySelector('body');
+let timer = null;
+
+startBtn.addEventListener("click", () => {
+  timer = setInterval(() => {body.style.backgroundColor =  getRandomHexColor(); }, 1000)
+  startBtn.setAttribute('disabled', true); stopBtn.removeAttribute('disabled')
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+stopBtn.addEventListener("click", () => { clearInterval(timer); stopBtn.setAttribute('disabled', true);  startBtn.removeAttribute('disabled')})
