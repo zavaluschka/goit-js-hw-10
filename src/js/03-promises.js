8be654972336456b0a50d224f76a4677d1b5b3da
zavaluschka <38804687+zavaluschka@users.noreply.github.com>
2023-08-25 09:17:02 +0300

    home003

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const firstDelay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + step * i;
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+});
